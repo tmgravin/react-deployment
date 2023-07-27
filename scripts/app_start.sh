4ce4542880d7108e4fd43057216f3b63e7fb253b
tmgravin <tmgravin@gmail.com>
2023-07-27 16:05:00 +0545

    new change

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/server/src
-npm install
-pm2 start npm - name "my-app" - start
-pm2 save
+npm start
+pm2 start npm --name "my-app" -- start
 pm2 startup
+pm2 save
 pm2 restart all
